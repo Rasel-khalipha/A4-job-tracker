e1a7925a2504caff4a6db528479f875a7e3356b6
Rasel-Khalipha <80430597+Rasel-khalipha@users.noreply.github.com>
2026-02-25 02:09:00 +0600

    job tracker functionality coding start here first variable call done

--- script/index.js
+++ script/index.js
@@ -1,1 +1,17 @@
 console.log("I am coming from index");
+
+let interviewList = [];
+let rejectedList = [];
+
+let totalCount = document.querySelectorAll(".total-count");
+let interviewCount = document.getElementById("interview-count");
+let rejectedCount = document.getElementById("rejected-count");
+
+const allBtn = document.getElementById("all-btn");
+const interviewBtn = document.getElementById("interview-btn");
+const rejectedBtn = document.getElementById("rejected-btn");
+
+const cardContainer = document.getElementById("card-container");
+const filterSection = document.getElementById("filter-section");
+
+const dynamicData = document.querySelector(".available .right");
